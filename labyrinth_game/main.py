fdc064607289c0b966fa1e00ff91cc38ed4395a9
subterraneanbob <4823336+subterraneanbob@users.noreply.github.com>
2025-09-27 20:24:43 +0300

    Define game state

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+game_state = {
+    "player_inventory": [],  # Инвентарь игрока
+    "current_room": "entrance",  # Текущая комната
+    "game_over": False,  # Значения окончания игры
+    "steps_taken": 0,  # Количество шагов
+}
+
 
 def main():
     print("Первая попытка запустить проект!")
